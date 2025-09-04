d590b9de162772bdff0c0fe652fcc8a4ba168d64
sthefany cruz <sthefany.cruz@escola.pr.gov.br>
2025-09-04 10:54:16 -0300

    .

---
+++ skitch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+let xJogador = [0, 0, 0, 0];
+let yJogador = [75, 150, 225, 300];
+let jogador = ["😎", "❤️", "👽", "🚘"];
+let teclas = ["a", "s", "d", "f"];
+let quantidade = jogador.length;
+
+function draw() {
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
+}
+
+function ativaJogo() {
+  if (focused == true) {
+    background("#D2EBB5");
+  } else {
+    background("rgb(238,178,178)");
+  }
+}
+
+function desenhaJogadores() {
+  textSize(40);
